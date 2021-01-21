b9019836e3438ef4c38bedb8147d35de5dabc564
Sfailla <Sfailla1983@gmail.com>
2021-01-20 22:14:50 -0500

    added styling for pricing page. finished price component. started compare component

---
+++ src/js/pricing.js
@@ -0,0 +1,33 @@
+import '../scss/main.scss';
+
+window.addEventListener('load', () => {
+  (function handleToggle() {
+    const input = document.querySelector('#checkbox');
+    const monthly = document.querySelector('.option1');
+    const yearly = document.querySelector('.option2');
+
+    const monthlyPrice = document.querySelectorAll('.price--monthly');
+    const yearlyPrice = document.querySelectorAll('.price--yearly');
+
+    input.addEventListener('click', () => {
+      if (monthly.classList.contains('active')) {
+        monthly.classList.remove('active');
+        yearly.classList.add('active');
+
+        monthlyPrice.forEach(price => (price.style.animationName = 'fade-out'));
+        monthlyPrice.forEach(price => (price.style.opacity = '0'));
+
+        yearlyPrice.forEach(price => (price.style.animationName = 'fade-in'));
+        yearlyPrice.forEach(price => (price.style.opacity = '1'));
+      } else {
+        yearly.classList.remove('active');
+        monthly.classList.add('active');
+
+        monthlyPrice.forEach(price => (price.style.animationName = 'fade-in'));
+        monthlyPrice.forEach(price => (price.style.opacity = '1'));
+        yearlyPrice.forEach(price => (price.style.animationName = 'fade-out'));
+        yearlyPrice.forEach(price => (price.style.opacity = '0'));
+      }
+    });
+  })();
+});
